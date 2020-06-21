e8f486439be20f5904e6a275a5365027feb72207
git <git@localhost.localdomain>
2020-06-21 21:27:39 +0530

    Basic crud with search etc

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
 from django.conf import settings
-
+from django.utils import timezone
 # Create your models here.
+
+class PostManager(models.Manager):
+
+    ## Returns all the posts which are not in draft state and publish date is less than or equal to current time
+    def active(self, *args, **kwargs):
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
 
 def upload_location(instance, filename):
     return ''.format(instance.pk, filename)
@@ -27,6 +33,7 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True,auto_now=False)
     updated   = models.DateTimeField(auto_now=True,auto_now_add=False)
 
+    objects = PostManager()
     def __str__(self):
         return self.title
 
--- posts/views.py
+++ posts/views.py
@@ -4,7 +4,8 @@ from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 from django.core.paginator import Paginator
-
+from django.utils import timezone
+from django.db.models import Q
 # Create your views here.
 
 def post_create(request):
@@ -24,6 +25,9 @@ def post_create(request):
 
 def post_detail(request,slug_id=None):
     instance = get_object_or_404(Post,  slug=slug_id)
+    if instance.draft : #or instance.publish >= timezone.now().date:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content, safe='', encoding=None, errors=None)# urlparse(instance.content)
     context = {
         "title": instance.title,
@@ -33,7 +37,20 @@ def post_detail(request,slug_id=None):
     return render(request, 'post_details.html', context)
 
 def post_list(request):
-    qs = Post.objects.all().order_by('-timestamp')
+    today = timezone.now().date()
+    if request.user.is_staff or request.user.is_superuser:
+        qs = Post.objects.all().order_by('-timestamp')
+    else:
+        qs = Post.objects.active().order_by('-timestamp')
+    query = request.GET.get('q')
+    if query:
+        qs = qs.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+       #     Q(user__name__icontains=query) #|
+            Q(user__first_name__icontains=query) |
+           Q(user__last_name__icontains=query)
+        ).distinct()
     paginator = Paginator(qs, 5)
     page_request_var = 'pageindex'
     page_number = request.GET.get(page_request_var)
@@ -41,7 +58,8 @@ def post_list(request):
     context = {
         "title": "Blogs",
         "object_list" : page_obj,
-        "page_request_var" : page_request_var
+        "page_request_var" : page_request_var,
+        'today' : today
     }
     return render(request, 'post_list.html', context)
 
