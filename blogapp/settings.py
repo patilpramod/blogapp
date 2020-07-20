c48151322cb0239955b913dcf1e77ecf3af66fd5
git <git@localhost.localdomain>
2020-07-20 12:18:56 +0530

    Enabled the Markdown

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    ##Thirdparty apps
+    'pagedown.apps.PagedownConfig',
+
     #MyModules
     'posts',
 ]
--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Post
+from pagedown.widgets import PagedownWidget
+
 
 class PostForm(forms.ModelForm):
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+    content =  forms.CharField(widget=PagedownWidget)
     class Meta:
         model = Post
         fields = [
